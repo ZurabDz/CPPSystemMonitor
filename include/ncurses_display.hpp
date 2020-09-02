08f4596fce047aaaa668d44b18c7af22e93ecbfd
ZurabDz <zurazuriko107@gmail.com>
2020-09-02 17:23:54 +0400

    DisplayProcesses should have access on system generally hence passing it instead of weird vector

--- include/ncurses_display.hpp
+++ include/ncurses_display.hpp
@@ -12,7 +12,7 @@
 namespace NCursesDisplay {
 void Display(System& system, int n);
 void DisplaySystem(System& system, WINDOW* window);
-void DisplayProcesses(std::vector<Process>& processes, WINDOW* window, int n);
+void DisplayProcesses(System& system, WINDOW* window);
 std::string ProgressBar(double percent);
 }  // namespace NCursesDisplay
 
