bd49e2c234f9240a2eb46cb5b463512f27f07b3b
Zurab Dzindzibadze <zuradzindzibadze06@gmail.com>
2020-09-16 19:16:40 +0400

    Added catch2 for unittest support

--- test/validate_parser.cpp
+++ test/validate_parser.cpp
@@ -1,3 +1,13 @@
 //
 // Created by penguin on 9/15/20.
 //
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch2.h"
+#include "../include/linux_parser.hpp"
+
+
+TEST_CASE("CPU CORE AMOUNT IS PARSED", "[GetCpuCores]") {
+  REQUIRE(LinuxParser::GetNumberOfCores() == 2);
+}
